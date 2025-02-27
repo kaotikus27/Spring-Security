1281fde8ce9a9b6f69a473eecb30e06802c862c7
ELEOMAR <kaotikus27@gmail.com>
2025-02-27 10:56:48 +0800

    dependencies for JWT added

--- src/main/java/com/example/security/SecurityConfig.java
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -54,13 +56,13 @@ public class SecurityConfig {
 
         UserDetails user1 =
                 User.withUsername("user1")
-                        .password("{noop}password1")
+                        .password(passwordEncoder().encode("password1"))
                         .roles("USER")
                         .build();
 
         UserDetails admin =
                 User.withUsername("admin")
-                        .password("{noop}admin1")
+                        .password(passwordEncoder().encode("admin1"))
                         .roles("ADMIN")
                         .build();
 
@@ -72,5 +74,11 @@ public class SecurityConfig {
         userDetailsManager.createUser(admin);
 
         return userDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
     }
 }
